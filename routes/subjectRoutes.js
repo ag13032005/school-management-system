c398bc7e699cca721ecd64dd58fe8d6fba6e51d0
Akash Gupta <66068737+ag13032005@users.noreply.github.com>
2025-04-27 21:46:31 +0530

    Update subjectRoutes.js

--- routes/subjectRoutes.js
+++ routes/subjectRoutes.js
@@ -145,20 +145,3 @@ exports.deleteGrade = async (req, res) => {
     res.redirect('/standards');
   }
 };
-
-// routes/gradeRoutes.js - Grade routes
-const express = require('express');
-const router = express.Router();
-const gradeController = require('../controllers/gradeController');
-const { isTeacherAuthenticated } = require('../middleware/auth');
-
-// Apply authentication middleware to all routes
-router.use(isTeacherAuthenticated);
-
-// Grade management
-router.get('/:standardId', gradeController.getGrades);
-router.post('/add', gradeController.addGrade);
-router.post('/edit/:id', gradeController.updateGrade);
-router.get('/delete/:id', gradeController.deleteGrade);
-
-module.exports = router;
