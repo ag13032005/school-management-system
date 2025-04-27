05bcb3eee3c78061d9fc44f2e86ed1a5796840d6
Akash Gupta <66068737+ag13032005@users.noreply.github.com>
2025-04-27 21:35:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const flash = require('connect-flash');
 const path = require('path');
-const port = process.env.PORT || 8080;
 
 // Initialize app
 const app = expres();
